532e90510f6f5d244dc8cab8cf7165bf8e2cb09f
chenbainian <284049379@qq.com>
2020-07-18 23:48:00 +0800

    登录之后显示头像信息和退出登录功能

--- front/api/index.js
+++ front/api/index.js
@@ -1,7 +1,7 @@
 import request from '@/utils/request'
 
 export default {
-    //查询热门课程和名师
+  //查询热门课程和名师
   getIndexData() {
     return request({
       url: '/edu/service/banner-front/index',
--- front/pages/login.vue
+++ front/pages/login.vue
@@ -22,9 +22,8 @@
             <i class="iconfont icon-password"/>
           </div>
         </el-form-item>
-
         <div class="btn">
-          <input type="button" class="sign-in-button" value="登录" @click="submitLogin()">
+          <input type="button" class="sign-in-button" value="登录" @click="submitLogin">
         </div>
       </el-form>
       <!-- 更多登录方式 -->
@@ -44,9 +43,9 @@
   import '~/assets/css/sign.css'
   import '~/assets/css/iconfont.css'
   import cookie from 'js-cookie'
+  import loginApi from '@/api/login'
   export default {
     layout: 'sign',
-
     data () {
       return {
         user:{
@@ -56,11 +55,27 @@
         loginInfo:{}
       }
     },
-
     methods: {
-
+      //登录的方法
+      submitLogin() {
+        //第一步 调用接口进行登录，返回token字符串
+        loginApi.submitLoginUser(this.user)
+          .then(response => {
+            //第二步 获取token字符串放到cookie里面
+            //第一个参数cookie名称，第二个参数值，第三个参数作用范围
+            cookie.set('guli_token',response.data.data.token,{domain: 'localhost'})
+            //第四步 调用接口 根据token获取用户信息，为了首页面显示
+            loginApi.getLoginUserInfo()
+              .then(response => {
+                this.loginInfo = response.data.data.userInfo
+                //获取返回用户信息，放到cookie里面
+                cookie.set('guli_ucenter',this.loginInfo,{domain: 'localhost'})
+                //跳转页面
+                window.location.href = "/";
+              })
+          })
+      },
       checkPhone (rule, value, callback) {
-        //debugger
         if (!(/^1[34578]\d{9}$/.test(value))) {
           return callback(new Error('手机号码格式不正确'))
         }
