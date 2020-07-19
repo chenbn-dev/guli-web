b4d0654d1aaef9c5469896a96a161f8ffed44961
chenbainian <284049379@qq.com>
2020-07-19 23:57:53 +0800

    课程列表和详情显示

---
+++ front/api/course.js
@@ -0,0 +1,27 @@
+import request from '@/utils/request'
+import id from "element-ui/src/locale/lang/id";
+
+export default {
+  //查询课程条件分页
+  getCourseList(page,limit,searchObj) {
+    return request({
+      url: `/edu/service/coursefront/getFrontCourseList/${page}/${limit}`,
+      method: 'post',
+      data: searchObj
+    })
+  },
+  // 查询所有分类的方法
+  getAllSubject() {
+    return request({
+      url: `/edu/service/subject/getAllSubject`,
+      method: 'get'
+    })
+  },
+  // 课程详情的方法
+  getCourseInfo() {
+    return request({
+      url: `/edu/service/coursefront/getFrontCourseInfo/${id}`,
+      method: 'get'
+    })
+  }
+}
