79792acc935129a65b7a3df733072f516bdb52d8
chenbainian <284049379@qq.com>
2020-07-29 00:18:55 +0800

    生成统计数据

---
+++ admin/src/api/sta.js
@@ -0,0 +1,18 @@
+import request from '@/utils/request'
+
+export default {
+  // 1 生成统计数据
+  createStaData(day) {
+    return request({
+      url: '/edu/statistics/daily/registerCount/' + day,
+      method: 'post'
+    })
+  },
+  // 2 获取统计数据
+  getDataSta(searchObj) {
+    return request({
+      url: `/edu/statistics/daily/showData/${searchObj.type}/${searchObj.begin}/${searchObj.end}`,
+      method: 'get'
+    })
+  }
+}
